d91d1f77217feb531c81a63619cdefa49d4d1703
Justus Kelsch <33078472+JustusKelsch@users.noreply.github.com>
2023-01-12 00:47:12 -0700

    Added a button that prompts the user to specify the amount of squares per row they would like.
    Added random colors to each square on the initial mouse over.
    Each additional mouseover adds 10% more black to the square until it is
    completly black after 15 mouseovers.
    
    Added some more styling that centers the grid and the button to the
    center of the screeen.
    
    Further documented the code to assist in future readability.

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ container.style.backgroundColor = 'red';
 container.style.padding = '10px';
 
 function createGrid(numPerSide) {
-    let squares = numPerSide * numPerSide;
+    let squares = 0;
+    if (numPerSide <= 100 && numPerSide > 0) {
+        squares = numPerSide ** 2;
+    }
+    else {
+        numPerSide = 16;
+        squares = 256;
+    }
+
+    // Creates a enough squares to create a grid based on the 
+    // squares per row the user entered.
     for (let i = 1; i <= squares; i++) {
         var div = document.createElement('div');
         div.classList.add('square');
@@ -15,16 +25,58 @@ function createGrid(numPerSide) {
         container.appendChild(div)
     } 
 
+    // Sets a random color for each square when moused over.
+    // The color slowly darkens until it is completely black.
+    document.querySelectorAll('.square').forEach(div => {
+        let counter = 0;
+        let x = getRandNum();
+        let y = getRandNum();
+        let z = getRandNum();
+        div.addEventListener('mouseover', e => {
+
+            // Less code length but takes much longer for squares to turn black
+            // div.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
+            // x = x - (x * .1);
+            // y = y - (y * .1);
+            // z = z - (z * .1);
+
+            // Slightly more code but squares turn black after 15 passes.
+            if (counter < 14) {
+                div.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
+                x = x - (x * .1);
+                y = y - (y * .1);
+                z = z - (z * .1);
+            }
+            else {
+                div.style.backgroundColor = 'black';
+            }
+
+            counter++;
+        });
+    });
+
+    // Sets the total size of the grid to fill the space of the container div.
+    // Equally sizes each square div to fit inside the grid.
     var px = 1000 / numPerSide;
     container.style.setProperty('grid-template-columns', `repeat(${numPerSide}, ${px}px)`);
     container.style.setProperty('grid-template-rows', `repeat(${numPerSide}, ${px}px)`);
 
 }
 
-createGrid(16);
+function getRandNum() {
+    // Generates a random number between 0 and 255
+    return Math.floor(Math.random() * 255);
+}
 
-document.querySelectorAll('.square').forEach(div => {
-    div.addEventListener('mouseover', event => {
-        div.style.backgroundColor = 'black';
+// Clears the existing grid and creates a new grid to the number of squares
+// per row specified by the user.
+document.querySelector('button').addEventListener('click', e => {
+    let input = prompt('Please list the amount of squares you would like per row: ');
+    document.querySelectorAll('.square').forEach(div => {
+        div.remove();
     });
-})
+    createGrid(input);
+});
+
+// Sets the default grid to 16 x 16 squares.
+createGrid(16);
